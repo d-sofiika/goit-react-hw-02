bfe30e565918b2faab93cef469cf137918b8b0a9
d-sofiika <soniadrozd2709@gmail.com>
2024-06-05 16:28:58 +0200

    added new component Notification

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,25 @@ import "./App.css";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
 
+
+
+function App() {
+
+  const objData = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+};
 const createInitialCounter = () => {
   const localCounter = localStorage.getItem('feedback')
   if (localCounter) {
     return JSON.parse(localCounter)
   }
-  return 0
-}
-
-function App() {
+  return objData
+  }
+  
   const [feedback, setFeedback] = useState(createInitialCounter);
 
  useEffect(() => {
@@ -28,11 +37,7 @@ function App() {
   };
 
    const resetFeedback = () => {
-    setFeedback({
-      good: 0,
-      neutral: 0,
-      bad: 0
-    });
+    setFeedback(objData);
   };
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
@@ -53,7 +58,7 @@ function App() {
             interest={interest}
           /> 
         ) : (
-          "No feedback yet. You are first!"
+          <Notification/>
         )}
       </div>
     </>
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,6 @@
+const Notification = () => {
+    return (<p>No feedback yet. You are first!</p>)
+        
+}
+
+export default Notification
